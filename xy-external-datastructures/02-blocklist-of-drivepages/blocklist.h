20c7b5908566200a9842020e8c88e40db7f2d638
EMerckx <ewoutmerckx.pro@gmail.com>
2015-10-24 23:35:53 +0200

    blocklist add and operator<< added

--- xy-external-datastructures/02-blocklist-of-drivepages/blocklist.h
+++ xy-external-datastructures/02-blocklist-of-drivepages/blocklist.h
@@ -2,36 +2,130 @@
 #define BLOCKLIST_OF_DRIVEPAGES_BLOCKLIST_H
 
 #include <list>
-#include <tuple>
-#include <map>
+#include <iostream>
+#include <utility>
 #include "drive.h"
 
 using std::list;
-using std::map;
+using std::ostream;
 using std::pair;
+
+const int CAPACITY = 4;
 
 template<class Key, class Data>
 class DrivePage;
 
 template<class Key, class Data>
 class BlockList {
-public:
-    BlockList(Drive<DrivePage<Key, Data> > &drive, BlockIndex blockIndex) : drive(&drive), blockIndex(blockIndex) { }
-
 private:
     BlockIndex blockIndex;
     Drive<DrivePage<Key, Data>> *drive;
+
+public:
+    BlockList(Drive<DrivePage<Key, Data> > &drive) : drive(&drive) {
+        DrivePage<Key, Data> drivePage;
+        blockIndex = drive.write(drivePage);
+    }
+
+    BlockList(Drive<DrivePage<Key, Data> > &drive, BlockIndex blockIndex) : drive(&drive), blockIndex(blockIndex) { }
+
+    void Add(Key key, Data data);
+
+    // operator << to show the BlockList on screen
+    friend ostream &operator<<(ostream &os, BlockList &blockList) {
+        // if the Drive and the BlockIndex exist
+        if (blockList.drive != 0 && blockList.blockIndex != 0) {
+            // create a local DrivePage
+            DrivePage<Key, Data> drivePage;
+            // read the current DrivePage from the drive
+            BlockIndex bi = blockList.blockIndex;
+
+            while (bi != 0) {
+                // read the the DrivePage
+                blockList.drive->read(drivePage, bi);
+
+                // write the DrivePage to the stream
+                os << drivePage;
+
+                // get the BlockIndex of the next DrivePage
+                bi = drivePage.next_blockIndex;
+            }
+        }
+        return os;
+    }
+
+    void write(ostream) const;
 };
 
-const int CAPACITY = 4;
-
 template<class Key, class Data>
-class DrivePage : public map<Key, Data> {
+class DrivePage : public list<pair<Key, Data> > {
 public:
     BlockIndex next_blockIndex;
 
-    DrivePage() : next_blockIndex(-1) { }
+    DrivePage() : next_blockIndex(0) { }
+
+    bool tryAdd(Key key, Data data);
+
+    // operator << to show the DrivePage on screen
+    friend ostream &operator<<(ostream &os, DrivePage &drivePage) {
+        for (auto it = drivePage.begin(); it != drivePage.end(); it++) {
+            os << "(" << it->first << "," << it->second << ") ";
+        }
+        os << "| ";
+        return os;
+    }
 };
+
+//---------------------------------------------------------------------------------------------------------------------
+
+template<class Key, class Data>
+void BlockList<Key, Data>::Add(Key key, Data data) {
+    // if the Drive and the BlockIndex exist
+    if (drive != 0 && blockIndex != 0) {
+        // create a local DrivePage
+        DrivePage<Key, Data> drivePage;
+        // read the current DrivePage from the drive
+        BlockIndex bi = this->blockIndex;
+        drive->read(drivePage, bi);
+
+        // while adding the key and data fails, read the next DrivePage
+        // or create a new one if the next DrivePage doesn't exist
+        while (!drivePage.tryAdd(key, data)) {
+            // save the BlockIndex of the current DrivePage
+            BlockIndex currentBlockIndex = bi;
+
+            // get the BlockIndex of the next DrivePage
+            bi = drivePage.next_blockIndex;
+            if (bi == 0) {
+                // create a new DrivePage
+                DrivePage<Key, Data> newDrivePage;
+                bi = drive->write(newDrivePage);
+
+                // set the BlockIndex of the next DrivePage
+                // and update it on the drive
+                drivePage.next_blockIndex = bi;
+                drive->rewrite(drivePage, currentBlockIndex);
+            }
+            drive->read(drivePage, bi);
+        }
+
+        // rewrite the DrivePage to finally add the Key and Data
+        drive->rewrite(drivePage, bi);
+    }
+}
+
+
+template<class Key, class Data>
+bool DrivePage<Key, Data>::tryAdd(Key key, Data data) {
+    if (this->size() < CAPACITY) {
+        pair<Key, Data> p(key, data);
+        this->push_back(p);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
 #endif //BLOCKLIST_OF_DRIVEPAGES_BLOCKLIST_H
--- xy-external-datastructures/02-blocklist-of-drivepages/main.cpp
+++ xy-external-datastructures/02-blocklist-of-drivepages/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include "blocklist.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int main() {
     cout << "Compilation succeeded" << endl;
 
+    // create the components of the BlockList
     Drive<DrivePage<int, int>> drive;
     DrivePage<int, int> drivePage;
     BlockIndex blockIndex = drive.write(drivePage);
 
+    // create the BlockList
     BlockList<int, int> blockList(drive, blockIndex);
+
+    // add some values to the BlockList
+    for (int i = 0; i < 15; i++) {
+        blockList.Add(i, i);
+    }
+
+    cout << blockList << endl;
+
     return 0;
 }
